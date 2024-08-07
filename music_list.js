b97925b571006c425d041da38786c529cd4b2c26
spider1215 <spider1215@naver.com>
2024-08-07 16:10:10 +0900

    초기버전
    초기버전

---
+++ music_list.js
@@ -0,0 +1,8 @@
+const musicList = [
+    {name: "Alone", artist: "Color Out" , audio:"Alone", img:"album1", total:"04:05"},
+    {name: "Mahidevran", artist: "Mahidevran rock band" , audio:"Mahidevran - Maze_of_sorrow", img:"album2", total:"06:28"},
+    {name: "No Rest Or Endless Rest", artist: "Lisofv" , audio:"No_Rest_Or_Endless_Rest -Lisofv", img:"album3", total:"04:33"},
+    {name: "Old Melody", artist: "TAB" , audio:"Old_Melody-TAB", img:"album4", total:"04:01"},
+    {name: "Tantalizing Youth", artist: "Social Square" , audio:"Tantalizing Youth - Social Square", img:"album5", total:"04:12"},
+    {name: "The Deep", artist: " Anitek" , audio:"The Deep - Anitek", img:"album6", total:"02:24"},
+]
